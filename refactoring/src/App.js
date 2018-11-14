c619d1b03d2f3da9df4d1e6d1390b041959575fc
Gerardo Fernandez <gerardojose13@gmail.com>
2018-11-14 07:51:54 -0800

    adds Header and Navbar components

--- refactoring/src/App.js
+++ refactoring/src/App.js
@@ -1,25 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar />
+        <Header />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac
+          tincidunt nisi. Quisque eget mollis mi, sit amet interdum lacus. Orci
+          varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Morbi at arcu in sem viverra laoreet ut ac nisl.
+          Praesent enim elit, tempus id tincidunt eget, elementum at erat. Sed
+          venenatis augue non velit rutrum, in blandit dolor vestibulum. Nunc ac
+          semper augue. Duis gravida eleifend tincidunt. Aenean vehicula iaculis
+          sagittis. Suspendisse pulvinar dui metus. Fusce fringilla arcu
+          tincidunt consequat convallis. Etiam tincidunt ultricies sapien vitae
+          semper. Phasellus ultrices consequat orci vel imperdiet. In ultricies
+          tortor eget purus congue, sed tincidunt ipsum ultrices. Nunc varius
+          consequat lectus, nec ultrices justo volutpat et. Nullam nec gravida
+          ligula. Quisque dictum odio in sapien fermentum, ac aliquam est
+          finibus. In hac habitasse platea dictumst. Donec vitae nulla nibh.
+          Integer porttitor facilisis nisi et aliquam. Etiam nec ex mauris.
+          Suspendisse gravida nisi quis placerat elementum. Sed sapien lacus,
+          ultricies eget felis id, ornare maximus sem. Etiam fringilla faucibus
+          dignissim. Donec ex ipsum, volutpat eget ex et, rhoncus blandit ante.
+          Fusce diam ex, pulvinar facilisis dolor pretium, consectetur fermentum
+          nunc. Nulla facilisi. Donec hendrerit pellentesque ante et fermentum.
+          Etiam rutrum leo vitae felis sagittis, at laoreet ex elementum.
+          Phasellus eget erat vel nulla placerat dapibus. Donec et lorem a nisl
+          vulputate feugiat. Proin luctus diam quis nulla lacinia scelerisque.
+          In vel feugiat ipsum. Morbi consequat sapien quis mi pharetra
+          consectetur. Nam vel sollicitudin risus, in porttitor turpis. Proin
+          vitae tempor dui. Nam mollis semper lectus sed dignissim. Nunc lorem
+          nunc, lobortis a lacinia eget, bibendum a mi. Integer condimentum
+          varius ultrices. Proin volutpat convallis leo, vitae ultricies libero.
+          Donec volutpat tellus ut risus pharetra, at volutpat augue tempor.
+          Morbi placerat fermentum erat, sit amet egestas ex auctor non. Donec
+          vehicula eu ligula vel placerat. Mauris nec dapibus massa, a blandit
+          diam. Nunc venenatis condimentum consequat. Donec vulputate risus
+          molestie feugiat ultricies. Nunc pretium, leo quis vestibulum
+          venenatis, metus mi faucibus libero, eget placerat risus urna vitae
+          nibh. Nunc in sapien libero. Ut porttitor eget risus vitae maximus.
+          Vivamus id lorem arcu. Praesent congue urna est, et eleifend ante
+          laoreet sit amet. Nam eu posuere nisi. Vivamus pretium odio nec ante
+          consequat, et viverra nibh feugiat. Phasellus auctor imperdiet lectus,
+          sed cursus enim iaculis in. Sed hendrerit tortor leo, quis tempus sem
+          imperdiet id. Nulla eu lacus id augue finibus luctus a eu mi. Aenean
+          ullamcorper in justo ac condimentum. Cras vitae ligula neque. Aliquam
+          pulvinar non nisi et sollicitudin. Morbi eget blandit sem. Nam
+          sagittis pulvinar velit id aliquam. Nam ipsum tortor, viverra eget
+          dapibus non, fermentum vel lorem. Vestibulum ac dolor diam. Nunc ac
+          nisi et nunc eleifend viverra at maximus enim. Aenean volutpat enim at
+          magna gravida viverra. Integer ex massa, viverra sed ultrices quis,
+          euismod ut urna. Phasellus quis mi sit amet risus posuere mattis. Nunc
+          nec efficitur lectus. Nunc tempor orci vel tellus volutpat venenatis.
+          Phasellus ipsum massa, faucibus sit amet viverra vitae, posuere at ex.
+          Pellentesque vitae erat id felis feugiat tincidunt. Nunc gravida,
+          felis vel dapibus euismod, est urna blandit mauris, vitae pharetra
+          justo tortor ac justo. Nunc ultrices nunc sit amet blandit porta. Sed
+          ut urna at ipsum faucibus placerat accumsan nec massa. Suspendisse ac
+          metus sed turpis hendrerit finibus. Vestibulum venenatis ac nisl a
+          congue. In hac habitasse platea dictumst. Nulla fringilla facilisis
+          venenatis. Ut eros mi, auctor et tellus non, ornare finibus mauris.
+          Fusce molestie consequat ipsum, sit amet luctus orci ultricies sit
+          amet. Class aptent taciti sociosqu ad litora torquent per conubia
+          nostra, per inceptos himenaeos. Mauris ut nibh ac magna ultrices
+          ullamcorper. Morbi neque libero, ornare et ante et, accumsan ultrices
+          ante. Sed porta placerat lorem. Quisque elementum scelerisque nunc.
+          Integer ut sagittis nibh. Nam tempor, magna quis iaculis ultrices,
+          felis lorem feugiat massa, ac hendrerit lacus dolor sit amet enim. In
+          pretium egestas felis vitae sodales. Curabitur interdum sodales eros,
+          ac pellentesque sapien viverra at. Cras in turpis eros. Integer
+          faucibus fermentum felis, eu malesuada sapien hendrerit a. Aenean
+          fermentum odio ut lorem faucibus, sit amet ullamcorper arcu efficitur.
+          Curabitur fringilla vitae diam eget rutrum.
+        </p>
       </div>
     );
   }
---
+++ refactoring/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="first-header">
+      <h1>This is the Header</h1>
+    </div>
+  );
+};
+
+export default Header;
