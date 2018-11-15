77c740bfc9a89e087e7617e5376c946f8ee1768a
Gerardo Fernandez <gerardojose13@gmail.com>
2018-11-15 00:19:08 +0000

    refactors App component

--- refactoring/src/components/Header/Header.js
+++ refactoring/src/components/Header/Header.js
@@ -4,7 +4,14 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="header">
-      
+      <div className="container">
+        <h1 className="opening-header body-text">
+          Chat now.
+          <br />
+          Doctors available 24/7.
+        </h1>
+        <p className=" body-text">Connect with a doctor today</p>
+      </div>
     </div>
   );
 };
